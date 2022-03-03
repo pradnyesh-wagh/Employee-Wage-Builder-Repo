f28feff0d18c140de15ca6b173532911c3baec5a
pradnyesh-wagh <pswagh420@gmail.com>
2022-03-03 17:18:55 +0530

    ADDED: Employee Attendance Check

---
+++ src/com/igc/empwage/Empwagebuilder.java
@@ -0,0 +1,22 @@
+package com.igc.empwage;
+
+import java.util.Random;
+
+public class Empwagebuilder {
+    public static void main(String[] args) {
+
+        //wellcome sms for user
+        System.out.println("Well come To The Employee Wase Problem Devloped By Pradnyesh Wagh");
+        Random random = new Random();
+        int emp =random.nextInt()%2;
+
+        //Uc-1
+        System.out.println(emp);
+        if (emp==0){
+            System.out.println("Employee Is Absent:");
+        }else{
+            System.out.println("Employee Is Present:");
+        }
+    }
+
+}
