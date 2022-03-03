ed0a4f5d3c14ec4701eca4d9b933d7dff96f4636
pradnyesh-wagh <pswagh420@gmail.com>
2022-03-03 17:32:25 +0530

    ADDED: Daily Wage Calculation

--- src/com/igc/empwage/Empwagebuilder.java
+++ src/com/igc/empwage/Empwagebuilder.java
@@ -6,17 +6,24 @@ public class Empwagebuilder {
     public static void main(String[] args) {
 
         //wellcome sms for user
-        System.out.println("Well come To The Employee Wase Problem Devloped By Pradnyesh Wagh");
+        System.out.println("Welcome To The Employee Wage Problem Developed By Pradnyesh Wagh");
+        //UC-2 Calculate Per Day Wage
+        final int WAGE_PER_HR = 20;
+        final int FULL_DAY_HR = 8;
+        int totalWage = 0;
         Random random = new Random();
         int emp =random.nextInt()%2;
 
-        //Uc-1
+        //Uc-1 Check Employee Attendance
         System.out.println(emp);
         if (emp==0){
             System.out.println("Employee Is Absent:");
+            totalWage = 0;
         }else{
             System.out.println("Employee Is Present:");
+            totalWage = WAGE_PER_HR * FULL_DAY_HR;
         }
+        System.out.println("Total Wage:="+totalWage);
     }
 
 }
